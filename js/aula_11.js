d0b527a6445af94758a7d9f71b383ab3638e1091
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-07-17 06:01:46 -0300

    Aula_11.js

---
+++ js/aula_11.js
@@ -0,0 +1,20 @@
+// document.write não funciona.
+/*
+
+var vel = 59.5
+console.log(`Velocidade do carro é ${vel}Km/h`)     // tipo o print no python
+if (vel > 60) {     // Condição Simples
+    console.log('Velocidade permitida ULTRAPASSADA!')
+} else{     // Condição composta
+    console.log('Dirija com segurança.')    // comandos sequênciais.
+}
+
+*/
+
+var país = `Brasil`
+console.log(`Vivendo em ${país}`)
+if (país == `Brasil`) { 
+    console.log (`Você é brasileiro`)
+} else {
+    console.log(`Estrangeiro`)
+}
