442d3706e2eb77b90d08a9f44eaad1cbb941b668
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-07-21 12:17:15 -0300

    Repetições 1

---
+++ js/aula_13.js
@@ -0,0 +1,36 @@
+/* 
+//estrutura de repetição com Teste Lógico no início, mais encontrada na maioria dos programas.
+condition = 1 // pra contar de 1 a 6 tem que por o começo em 1, senão começa do 0.
+while (condition <= 6) {
+    console.log(`Passo ${condition}`) // interpolação por place holders ${}
+    condition ++
+}
+*/
+
+// Estrutura de repetição com Teste Lógico no final
+condition = 1
+do {
+    console.log(`do doo e dooodoo`)
+    condition ++;
+} while (condition < 6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+console.log("Tudo bem ?")
+*/
