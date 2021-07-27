0585cf4a1de66a82eb9a2de19b5ec1cdf0ed4cc1
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-07-27 11:18:11 -0300

    Tabuada

--- js/ex004/ex004.js
+++ js/ex004/ex004.js
@@ -1,19 +1,17 @@
-function tabuada() {
-    var num = document.getElementById(`txtnumero`)
-    var tab = document.getElementById(`seltab`)
-   
-    if (n.value.length == 0) {
-        window.alert('COLOQUE UM NÚMERO')
+function calcular() {
+    var num = document.getElementById('txtnumero')
+    var tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('INSIRA UM NÚMERO PELO MENOS !')
     } else {
-        var n = Number(num.value)
-        var c = 1
-        tab.inner = ''
+        let n = Number(num.value)
+        let c = 0
         while (c <= 10) {
-            var item = document.createElement('option')// criar elemento dentro de JS, esse aqui do tipo option.
+            let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
-            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
-    } // NÃO FUNCIONA!
+    }
 }
