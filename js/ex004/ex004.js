cb9e202a0c161bf3db8c71441ffd466fb4f33570
Temóteo Francisco <contatotemoteofsp@gmail.com>
2021-07-27 11:33:24 -0300

    Tabuada (Deu certo)

--- js/ex004/ex004.js
+++ js/ex004/ex004.js
@@ -7,11 +7,13 @@ function calcular() {
     } else {
         let n = Number(num.value)
         let c = 0
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}` // value pra php, toda option tem que ter um value pra identificação no servidor.
             tab.appendChild(item)
-            c++
+            c++ // incremento, nunca se esqueça
         }
     }
 }
